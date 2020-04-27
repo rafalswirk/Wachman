c8f46edf659144b03e074d62790fa82a2101711b
Rafal <rafalswirk@wp.pl>
2020-04-27 22:11:37 +0200

    Added base class for job reading

---
+++ Wachman/Utils/TimeCamp/TimeCampStatusReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wachman.Utils.TimeCamp
+{
+    class TimeCampStatusReader
+    {
+        private readonly string apiKey;
+
+        public TimeCampStatusReader(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        internal string GetCurrentJob()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
