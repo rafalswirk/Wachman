19b8f4e99213c5816d21dff3f96375852e1fe55c
Rafal <rafalswirk@wp.pl>
2020-04-28 07:59:23 +0200

    - Added class for time camp status read

--- Wachman/Utils/TimeCamp/TimeCampStatusReader.cs
+++ Wachman/Utils/TimeCamp/TimeCampStatusReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,8 @@ namespace Wachman.Utils.TimeCamp
 
         internal string GetCurrentJob()
         {
+            var client = new RestClient(@"https://www.timecamp.com/third_party/api");
+            var request = new RestRequest(Method.GET);
             throw new NotImplementedException();
         }
     }
