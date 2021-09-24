f6484b307cd526d91f979778e25ff5ac02a2218d
Rafal <rafalswirk@outlook.com>
2021-09-24 21:25:26 +0200

    - Blur effect for active timer window

--- Wachman/MainWindow.xaml.cs
+++ Wachman/MainWindow.xaml.cs
@@ -1,28 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 using System.Windows.Threading;
 using Wachman.Utils.TimeCamp;
 
 namespace Wachman
 {
+    internal enum AccentState
+    {
+        ACCENT_DISABLED = 0,
+        ACCENT_ENABLE_GRADIENT = 1,
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+        ACCENT_ENABLE_BLURBEHIND = 3,
+        ACCENT_INVALID_STATE = 4
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public int GradientColor;
+        public int AnimationId;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+
+    internal enum WindowCompositionAttribute
+    {
+        // ...
+        WCA_ACCENT_POLICY = 19
+        // ...
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
         private TimeCampStatusReader timeCampStatusReader;
         DispatcherTimer timer;
         DateTime startTime;
@@ -60,6 +86,28 @@ namespace Wachman
             });
         }
 
+        internal void EnableBlur(bool enabled)
+        {
+            var windowHelper = new WindowInteropHelper(this);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = enabled ? AccentState.ACCENT_ENABLE_BLURBEHIND : AccentState.ACCENT_DISABLED;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
         private void UpdateClock()
         {
             var ellpasedTime = workingTime - (DateTime.Now - startTime);
@@ -93,12 +141,12 @@ namespace Wachman
 
         private void Window_MouseEnter(object sender, MouseEventArgs e)
         {
-            Background = Brushes.White;
+            EnableBlur(true);
         }
 
         private void Window_MouseLeave(object sender, MouseEventArgs e)
         {
-            Background = Brushes.Transparent;
+            EnableBlur(false);
         }
 
         private void Window_LocationChanged(object sender, EventArgs e)
@@ -114,6 +162,11 @@ namespace Wachman
                 Top = lastTop;
                 Left = lastLeft;
             }
+        }
+
+        private void AcrylicWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            EnableBlur(false);
         }
     }
 }
