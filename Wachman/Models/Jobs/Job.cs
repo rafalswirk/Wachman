6396722689cc390df251e1a69ffc559c0cd42ffc
Rafal <rafalswirk@outlook.com>
2021-11-01 06:10:21 +0100

    - Showing example data on dashboard

---
+++ Wachman/Models/Jobs/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wachman.Models.Jobs
+{
+    public class Job
+    {
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+    }
+}
--- Wachman/Views/DashboardView.xaml.cs
+++ Wachman/Views/DashboardView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Wachman.Models.Jobs;
 
 namespace Wachman.Views
 {
@@ -22,6 +23,14 @@ namespace Wachman.Views
         public DashboardView()
         {
             InitializeComponent();
+            var dataItems = new List<Job>()
+            {
+                new Job{ Name = "ProgrammingKata", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22)},
+                new Job{ Name = "Mailing", Start = new DateTime(2021, 11, 1, 13, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22)},
+                new Job{ Name = "Tea time", Start = new DateTime(2021, 11, 1, 12, 3, 22)}
+            };
+
+            dataGrid.ItemsSource = dataItems;
         }
     }
 }
