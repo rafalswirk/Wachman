35da96807c88fc64231ca8632ab95130b33b4945
rafalswirk <rafalswirk@outlook.com>
2022-05-20 08:41:08 +0200

    - Changing Job model namespace to new one

--- DataModels/Jobs/Job.cs
+++ DataModels/Jobs/Job.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Wachman.Models.Jobs
+namespace DataModels.Jobs
 {
     public class Job
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime Start { get; set; }
         public DateTime Stop { get; set; }
         public TimeSpan Duration { get; set; }
