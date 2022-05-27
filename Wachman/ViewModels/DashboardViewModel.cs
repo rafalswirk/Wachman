97dcb977bd48861d491acbfd3eb9f9eac7a079e3
rafalswirk <rafalswirk@outlook.com>
2022-05-27 07:29:49 +0200

    - Data loading moved to view model

--- Wachman/ViewModels/DashboardViewModel.cs
+++ Wachman/ViewModels/DashboardViewModel.cs
@@ -5,16 +5,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTrackingService.TimeCampAPI;
+using TimeTrackingService.TimeCampAPI.Client;
 
 namespace Wachman.ViewModels
 {
-    public class DashboardViewModel: ObservableObject
+    public class DashboardViewModel : ObservableObject
     {
+        private const bool HardcodedValues = true;
+
         private List<Job> _dailyJobs;
         public List<Job> DailyJobs
         {
-            get =>_dailyJobs;
+            get => _dailyJobs;
             set => SetProperty(ref _dailyJobs, value);
+        }
+
+        internal async Task OnLoaded()
+        {
+            if (HardcodedValues)
+            {
+                var dataItems = new List<Job>()
+                {
+                    new Job{ Name = "ProgrammingKata", Description = "Making fancy algorithm", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromMinutes(25)},
+                    new Job{ Name = "Mailing", Description = "Mail to office", Start = new DateTime(2021, 11, 1, 13, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromMinutes(72)},
+                    new Job{ Name = "Tea time", Description = "Green tea", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromSeconds(320)}
+                };
+
+                DailyJobs = dataItems;
+                return;
+            }
+
+            var dailyJobs = new GetDailyJobs(TimeCampApiClient.Instance);
+            DailyJobs = await dailyJobs.ExecuteAsync();
         }
 
     }
--- Wachman/Views/DashboardView.xaml.cs
+++ Wachman/Views/DashboardView.xaml.cs
@@ -24,35 +24,19 @@ namespace Wachman.Views
     /// </summary>
     public partial class DashboardView : Window
     {
-        private const bool HardcodedValues = true;
         private DashboardViewModel _dataContext;
 
         public DashboardView()
         {
             InitializeComponent();
+            TimeCampApiClient.Initialize(new ApiKeyProvider().GetKey());
             _dataContext = new DashboardViewModel();
-            DataContext = _dataContext;
-            if(HardcodedValues)
-            {
-                var dataItems = new List<Job>()
-                {
-                    new Job{ Name = "ProgrammingKata", Description = "Making fancy algorithm", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromMinutes(25)},
-                    new Job{ Name = "Mailing", Description = "Mail to office", Start = new DateTime(2021, 11, 1, 13, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromMinutes(72)},
-                    new Job{ Name = "Tea time", Description = "Green tea", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromSeconds(320)}
-                };
+            DataContext = _dataContext;      
+        }
 
-                _dataContext.DailyJobs = dataItems;
-            }
-            else
-            {
-                TimeCampApiClient.Initialize(new ApiKeyProvider().GetKey());
-                Application.Current.Dispatcher.InvokeAsync(async () =>
-                {
-                    var dailyJobs = new GetDailyJobs(TimeCampApiClient.Instance);
-                    var jobs = await dailyJobs.ExecuteAsync();
-                    _dataContext.DailyJobs = jobs;
-                });
-            }           
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _dataContext.OnLoaded();
         }
     }
 }
