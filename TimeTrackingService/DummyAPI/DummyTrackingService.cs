9ee73bb7d1f0e8e7a159cf79286c0469797fac6d
rafalswirk <rafalswirk@outlook.com>
2022-06-21 06:01:41 +0200

    - Showing job status button according to current job state

--- TimeTrackingService/DummyAPI/DummyTrackingService.cs
+++ TimeTrackingService/DummyAPI/DummyTrackingService.cs
@@ -15,7 +15,7 @@ namespace TimeTrackingService.DummyAPI
         {
             new Job{ Name = "ProgrammingKata", Description = "Making fancy algorithm", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromMinutes(25)},
             new Job{ Name = "Mailing", Description = "Mail to office", Start = new DateTime(2021, 11, 1, 13, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromMinutes(72)},
-            new Job{ Name = "Tea time", Description = "Green tea", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromSeconds(320)}
+            new Job{ Name = "Tea time", Description = "Green tea", Start = new DateTime(2021, 11, 1, 12, 3, 22), Stop = new DateTime(2021, 11, 1, 13, 3, 22), Duration = TimeSpan.FromSeconds(320), IsRunning = true}
         });
 
         public Task<string> GetCurrentJobName() => Task.FromResult("Pet project development");
---
+++ Wachman/DataTemplateSelectors/JobRunningTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using DataModels.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wachman.DataTemplateSelectors
+{
+    internal class JobRunningTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate JobRunningTemplate { get; set; }
+        public DataTemplate JobNotRunningTemplate { get; set; }
+        
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item == null)
+                return JobNotRunningTemplate;
+            if(item is Job job)
+            {
+                if(job.IsRunning)
+                    return JobRunningTemplate;
+            }
+            
+            return JobNotRunningTemplate;
+        }
+    }
+}
