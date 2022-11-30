f18c3102d943136b080b0fbfbe4875d1a16ff188
rafalswirk <rafalswirk@outlook.com>
2022-11-30 12:06:37 +0100

    - Countdown for the progressbar

--- Wachman/ViewModels/MicroBreakViewModel.cs
+++ Wachman/ViewModels/MicroBreakViewModel.cs
@@ -31,6 +31,15 @@ namespace Wachman.ViewModels
             set => SetProperty(ref _userMessage, value);
         }
 
+        private int _breakProgress;
+
+        public int BreakProgress
+        {
+            get => _breakProgress;
+            set => SetProperty(ref _breakProgress, value);
+        }
+
+
 
         public ICommand TakeBreak { get; set; }
         public ICommand SkipBreak { get; set; }
@@ -45,6 +54,7 @@ namespace Wachman.ViewModels
             TakeBreak = new RelayCommand(() => 
             {
                 IsBreakModeEnabled = true;
+                BreakProgress = 100;
                 UserMessage = $"{breakTime.Minutes:00}:{breakTime.Seconds:00}";
                 _startTime = DateTime.Now;
                 _timer.Interval = 1000;
@@ -53,6 +63,7 @@ namespace Wachman.ViewModels
                     var elpassedTime = DateTime.Now - _startTime;
                     var timeToFinish = breakTime - elpassedTime;
                     UserMessage = $"{timeToFinish.Minutes:00}:{timeToFinish.Seconds:00}";
+                    BreakProgress = (int)(100 - (elpassedTime.TotalSeconds * 100) / breakTime.TotalSeconds);
                 };
                 _timer.Start();
             });
