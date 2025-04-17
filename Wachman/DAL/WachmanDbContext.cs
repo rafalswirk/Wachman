8ab56b516ce9b8f245512c4690b3d9c5a8e43fff
rafalswirk <rafalswirk@outlook.com>
2025-04-17 17:15:23 +0200

    - Seed database with default pomodoro settings

--- Wachman/DAL/WachmanDbContext.cs
+++ Wachman/DAL/WachmanDbContext.cs
@@ -12,4 +12,15 @@ public class WachmanDbContext : DbContext
     {
         optionsBuilder.UseSqlite("Data Source=Wachman.db");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AppSetting>().HasData(
+            new AppSetting { SettingsKey = "WorkSessionDuration", SettingsValue = "30" },
+            new AppSetting { SettingsKey = "BreakTimeDuration", SettingsValue = "5" },
+            new AppSetting { SettingsKey = "DisableBreaks", SettingsValue = "0" }
+        );
+    }
 }
