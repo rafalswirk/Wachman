951b2a5d66e648e59755851a0dd99587f4b9437b
rafalswirk <rafalswirk@outlook.com>
2025-04-17 16:13:49 +0200

    - Add DisableBreaks property to configuration POCO

--- Wachman/Entities/PomodoroConfiguration.cs
+++ Wachman/Entities/PomodoroConfiguration.cs
@@ -6,4 +6,5 @@ public class PomodoroConfiguration
 {
     public int WorkSessionDuration { get; set; } = 30;
     public int BreakTimeDuration { get; set; } = 5;   
+    public bool DisableBreaks { get; set; } = false;
 }
