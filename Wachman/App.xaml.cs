79ea6565967b66d15adf11f0564464d8cc9bdd95
Rafał Świrk <rafalswirk@outlook.com>
2025-04-18 12:42:32 +0200

    - Apply database migration on application startup

--- Wachman/App.xaml.cs
+++ Wachman/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Wachman.DAL;
 
 namespace Wachman
 {
@@ -13,5 +14,9 @@ namespace Wachman
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            AutomatedMigrations.Apply();
+        }
     }
 }
---
+++ Wachman/DAL/AutomatedMigrations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wachman.DAL
+{
+    internal static class AutomatedMigrations
+    {
+        public static void Apply()
+        {
+            using var context = new WachmanDbContext();
+            context.Database.Migrate();
+        }
+    }
+}
