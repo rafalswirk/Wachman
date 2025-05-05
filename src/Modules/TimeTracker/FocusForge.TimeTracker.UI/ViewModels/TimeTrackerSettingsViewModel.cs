65dd5e4f128c8372eab5b30badd40daec7d8b9dd
Rafał Świrk <rafalswirk@outlook.com>
2025-05-05 09:25:17 +0200

    - Create marker interface for handling settings

--- src/Modules/TimeTracker/FocusForge.TimeTracker.UI/ViewModels/TimeTrackerSettingsViewModel.cs
+++ src/Modules/TimeTracker/FocusForge.TimeTracker.UI/ViewModels/TimeTrackerSettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FocusForge.DataModels.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FocusForge.TimeTracker.UI.ViewModels
 {
-    public class TimeTrackerSettingsViewModel
+    public class TimeTrackerSettingsViewModel : IModuleSettings
     {
     }
 }
