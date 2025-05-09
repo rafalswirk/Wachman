c770686a06583af85eeed09bbbf2bae212c92267
Rafał Świrk <rafalswirk@outlook.com>
2025-05-09 16:39:08 +0200

    - Store settings in module database

---
+++ src/Modules/TimeTracker/FocusForge.TimeTracker/DAL/TimeTrackerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FocusForge.DataModels.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.TimeTracker.DAL
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+        public DbSet<AppSetting> Settings { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionStringProvider.ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ src/Modules/TimeTracker/FocusForge.TimeTracker/Repositories/ITimeTrackerSettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.TimeTracker.Repositories
+{
+    internal interface ITimeTrackerSettingsRepository
+    {
+        string TimeCampApiKey { get; }
+        void SaveTimeCampApiKey(string timeCampApiKey);
+    }
+}
