e64cd0489db6ab138261e35a89194d7ab2016387
Rafał Świrk <rafalswirk@outlook.com>
2025-05-29 17:49:52 +0200

    - Fix build errors

---
+++ src/Modules/TimeTracker/FocusForge.TimeTracker.TimeCamp/FocusForge.TimeTracker.TimeCamp/ApiCommunication/ITimeTrackingCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.TimeTracker.TimeTrackingServiceCommand
+{
+    internal interface ITimeTrackingCommand<T>
+    {
+        Task<T> ExecuteAsync();
+    }
+}
