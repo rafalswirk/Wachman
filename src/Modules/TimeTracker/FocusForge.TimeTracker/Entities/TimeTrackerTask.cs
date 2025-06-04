f318eb119c9d389b3156de1864aca4bcb145d059
Rafał Świrk <rafalswirk@outlook.com>
2025-06-04 17:48:19 +0200

    - Create abstraction for storing time tracker tasks

---
+++ src/Modules/TimeTracker/FocusForge.TimeTracker/Entities/TimeTrackerTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.TimeTracker.Entities
+{
+    internal class TimeTrackerTask
+    {
+        public int Id { get; set; }
+        public int ExternalId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Modules/TimeTracker/FocusForge.TimeTracker/Repositories/ITimeTrackerTaskRepository.cs
@@ -0,0 +1,22 @@
+﻿using FocusForge.TimeTracker.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.TimeTracker.Repositories
+{
+    internal interface ITimeTrackerTaskRepository
+    {
+        Task<TimeTrackerTask> CreateTaskAsync(TimeTrackerTask task);
+
+        Task<TimeTrackerTask> GetTaskByIdAsync(string taskId);
+
+        Task<TimeTrackerTask> UpdateTaskAsync(TimeTrackerTask task);
+
+        Task<bool> DeleteTaskAsync(string taskId);
+
+        Task<IEnumerable<TimeTrackerTask>> GetAllTasksAsync();
+    }
+}
