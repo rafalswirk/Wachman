9a60e7c88a5ed1a9ebc3b4e046b27eec415b3530
Rafał Świrk <rafalswirk@outlook.com>
2025-08-10 12:56:24 +0200

    - Add reader for TimeCamp tasks

---
+++ src/Modules/TimeTracker/FocusForge.TimeTracker/Services/TimeTracking/TimeCampTasksDatabaseReader.cs
@@ -0,0 +1,24 @@
+﻿using FocusForge.TimeTracker.DAL;
+using FocusForge.TimeTracker.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.TimeTracker.Services.TimeTracking
+{
+    public class TimeCampTasksDatabaseReader : ITasksReader
+    {
+        private readonly TimeTrackerDbContext _context;
+
+        public TimeCampTasksDatabaseReader(TimeTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyCollection<TimeTrackerTask>> ReadAsync()
+            => await _context.TimeTrackerTasks.ToListAsync();
+    }
+}
