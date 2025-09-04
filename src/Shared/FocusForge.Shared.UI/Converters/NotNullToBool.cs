394d071345ca17624f409791388a04e9e10f5c1f
Rafał Świrk <rafalswirk@outlook.com>
2025-09-04 08:49:47 +0200

    - Don't allow to create new job if no one was selected

---
+++ src/Shared/FocusForge.Shared.UI/Converters/NotNullToBool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FocusForge.Shared.UI.Converters
+{
+    public class NotNullToBool : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is not null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
