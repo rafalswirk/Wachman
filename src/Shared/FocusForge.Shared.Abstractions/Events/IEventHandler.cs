83c8fd85e39037de2d278167c34409e20e8cda35
Rafał Świrk <rafalswirk@outlook.com>
2025-09-15 20:48:06 +0200

    - Add abstractions for events publishing and handling

---
+++ src/Shared/FocusForge.Shared.Abstractions/Events/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.Shared.Abstractions.Events
+{
+    public interface IEventHandler<in TEvent> : IEvent where TEvent :class, IEvent
+    {
+        Task HandleAsync(TEvent @event);
+    }
+}
