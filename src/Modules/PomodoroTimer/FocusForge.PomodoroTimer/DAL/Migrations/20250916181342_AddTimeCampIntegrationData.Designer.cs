24d1a6171286fae81141b9fd070aa01062489940
Rafał Świrk <rafalswirk@outlook.com>
2025-09-16 20:15:55 +0200

    - Add migration for default api key value

--- src/Modules/PomodoroTimer/FocusForge.PomodoroTimer/DAL/Migrations/20250916181342_AddTimeCampIntegrationData.Designer.cs
+++ src/Modules/PomodoroTimer/FocusForge.PomodoroTimer/DAL/Migrations/20250916181342_AddTimeCampIntegrationData.Designer.cs
@@ -2,16 +2,19 @@
 using FocusForge.PomodoroTimer.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Wachman.DAL.Migrations
+namespace FocusForge.PomodoroTimer.DAL.Migrations
 {
     [DbContext(typeof(WachmanDbContext))]
-    partial class WachmanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250916181342_AddTimeCampIntegrationData")]
+    partial class AddTimeCampIntegrationData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
