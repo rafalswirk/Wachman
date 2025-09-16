56b665a8437c6633bf39100a92329f07dde9b2d5
Rafał Świrk <rafalswirk@outlook.com>
2025-09-16 19:33:59 +0200

    - Prepare storage for api key

---
+++ src/Modules/PomodoroTimer/FocusForge.PomodoroTimer/DAL/Respositories/TimeCampIntegrationRepository.cs
@@ -0,0 +1,27 @@
+﻿using FocusForge.PomodoroTimer.Models;
+using FocusForge.PomodoroTimer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.PomodoroTimer.DAL.Respositories
+{
+    internal class TimeCampIntegrationRepository : ITimeCampIntegrationRepository
+    {
+        private readonly WachmanDbContext _context;
+
+        public TimeCampIntegrationRepository(WachmanDbContext context)
+        {
+            _context = context;
+        }
+        public TimeCampIntegrationData TimeCampApiKey
+            => throw new NotImplementedException();
+
+        public void SaveTimeCampIntegrationData(TimeCampIntegrationData timeCampApiKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Modules/PomodoroTimer/FocusForge.PomodoroTimer/Repositories/ITimeCampIntegrationRepository.cs
@@ -0,0 +1,15 @@
+﻿using FocusForge.PomodoroTimer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.PomodoroTimer.Repositories
+{
+    internal interface ITimeCampIntegrationRepository
+    {
+        TimeCampIntegrationData TimeCampApiKey { get; }
+        void SaveTimeCampIntegrationData(TimeCampIntegrationData integrationData);
+    }
+}
