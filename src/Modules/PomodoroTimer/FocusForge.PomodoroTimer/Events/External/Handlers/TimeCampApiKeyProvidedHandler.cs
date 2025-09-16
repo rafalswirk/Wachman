58d4681af5b96df0f27204f46180e8ab106eeaa3
Rafał Świrk <rafalswirk@outlook.com>
2025-09-16 17:37:32 +0200

    - Add TimeCampApiKeyProvided event with event handler

---
+++ src/Modules/PomodoroTimer/FocusForge.PomodoroTimer/Events/External/Handlers/TimeCampApiKeyProvidedHandler.cs
@@ -0,0 +1,18 @@
+﻿using FocusForge.Shared.Abstractions.Events;
+using FocusForge.TimeTracker.Messages.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.PomodoroTimer.Events.External.Handlers
+{
+    internal class TimeCampApiKeyProvidedHandler : IEventHandler<TimeCampApiKeyProvided>
+    {
+        public Task HandleAsync(TimeCampApiKeyProvided @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
