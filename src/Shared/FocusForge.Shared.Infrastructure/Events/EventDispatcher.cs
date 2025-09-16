8f638b966530c12b7fb1685f0f7917d015fc20ad
Rafał Świrk <rafalswirk@outlook.com>
2025-09-16 06:28:01 +0200

    - Add event dispatcher

---
+++ src/Shared/FocusForge.Shared.Infrastructure/Events/EventDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using FocusForge.Shared.Abstractions.Events;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.Shared.Infrastructure.Events
+{
+    public class EventDispatcher : IEventDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public EventDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task PublishAsync<TEvent>(TEvent @event) where TEvent : class, IEvent
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var handlers = scope.ServiceProvider.GetServices<IEventHandler<TEvent>>();
+
+            foreach (var handler in handlers)
+            {
+                await handler.HandleAsync(@event);
+            }
+        }
+    }
+}
