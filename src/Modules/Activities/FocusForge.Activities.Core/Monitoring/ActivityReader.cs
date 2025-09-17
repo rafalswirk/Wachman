7ffbe5397ede4b4571617957ead3bc819ea55933
Rafał Świrk <rafalswirk@outlook.com>
2025-09-17 15:03:40 +0200

    - Read current activity

---
+++ src/Modules/Activities/FocusForge.Activities.Core/Monitoring/ActivityReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusForge.Activities.Core.Monitoring
+{   
+    public class ActivityReader
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        public static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+                return Buff.ToString();
+
+            return string.Empty;
+        }
+    }
+}
--- src/Modules/Activities/FocusForge.Activities.UI/ViewModels/ActivitiesViewModel.cs
+++ src/Modules/Activities/FocusForge.Activities.UI/ViewModels/ActivitiesViewModel.cs
@@ -1,13 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FocusForge.Activities.Core.Monitoring;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace FocusForge.Activities.UI.ViewModels
 {
     public class ActivitiesViewModel : ObservableObject
     {
+        private string _currentActivity;
+        private System.Timers.Timer _timer;
+
+        public string CurrentActivity
+        {
+            get { return _currentActivity; }
+            set { SetProperty(ref _currentActivity, value); }
+        }
+
+        public ActivitiesViewModel()
+        {
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += (s, e) =>
+            {
+                CurrentActivity = ActivityReader.GetActiveWindowTitle();
+            };
+            _timer.Start();
+        }
     }
 }
